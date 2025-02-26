deddd9a042ef92db6b2c347d9d7a127d4f3c9874
Towhid <blatowhid@gmail.com>
2025-02-26 17:27:27 +0600

    modifying

---
+++ 003_what-is-repository.sh
@@ -0,0 +1,20 @@
+#1. remote repository
+#2. local repository
+# a repository is a folder that has a .git hidden file
+# each repository contain only 1 .git file
+# try to create or merge multiple .git(by init,pull or clone) it will be an Error
+#error name: unrelated history(cause each .git has different commit history)
+
+#to make a git repository we need to initialize git on that folder (for local)
+
+# afte initialization it will be a repository
+
+# the repo will have a .git hidden file
+
+#this .git control the version of the repository
+
+#to initilize ,we can run either
+git init
+#or, remotely create a repository and then,
+git clone <remote-repo-url>
+#note: by that , repository will be created in remotely then clone to local to edit
--- 006_initialize-repo.sh
+++ 006_initialize-repo.sh
@@ -1,26 +1,48 @@
-# a repository is a folder that has a .git hidden file
 #there is two way to initialize a repo 
-#1. clone a existing repo
-#2. init a local repo
-
-#1.
+#1. clone a existing repo    2. init(create) a local repo
+#1. clone=> each time a new
 git clone <repo-url>
-#2.
+#2. init=> only once in a folder
 git init
+#====================================
 
 #note: 1. locate the folder where you need to init or or clone the repo
 #note: to do that use cd in the terminal or manually open bash on the folder
-
 #note: BEST PRACTICE: use Vs Code and open the folder where to clone or open the folder where to initialize
 
 #difference:
+#1.
 git clone <repo-url>
-#clone the whole repository(folder) and the .git lies inside the repository
+clone BEST PRACTICE: can run in same or any folder with no limitation times.
+#it clone the whole repository(folder) and the .git lies inside the cloned repository not in the parent
 #we can clone more repo at the same parent folder and each cloned repository will have it's own .git(a own indivisual repository as a child)
 
+parent folder    #=> run( git clone <repo-url> )here again and again
+            first cloned repository
+                            - .git
+                            -other files
+            second cloned repository
+                            - .git
+                            -other files
+            third cloned repository
+                            - .git
+                            -other files
+note: each time run the clone command in the same parent folder . and it will clone a new folder as child repository
+
+
+
+#2.
 git init
+init BEST PRACTICE: run git init in different folder for different project only once each 
 #it make the .git folder at the same folder where we run the command
 #means the parent folder will have the .git (remember ,we can not initilize a git again after once)
 
+parent folder   #=> run ( git init)here only once
+            - .git
+            -repository1
+            -repository2
+            -repository3
+all repo will have the same .git #(but once)
+note:once run git init then can't run git init again
 
 
